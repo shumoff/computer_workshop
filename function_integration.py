5ceb89a73e1a8b011431b03066f02f8d3fc4a864
shumoff <dmitry.shumoff@gmail.com>
2019-04-08 03:03:28 +0300

    still problems with richardson

--- function_integration.py
+++ function_integration.py
@@ -48,8 +48,8 @@ def knot_polynomial_derivative(x, knots):
     return res
 
 
-def estimator_func(x, knots):
-    return abs(weight_func(x, 0) * knot_polynomial(x, knots))
+def estimator_func(x, knots, power=1):
+    return abs(weight_func(x, 0) * knot_polynomial(x, knots)**power)
 
 
 def coefficients_func(x, knots, j):
@@ -71,7 +71,15 @@ def regular_iqr(a, b, n):
             knots_matrix[s][j] = knots[j] ** s
     moments_vector = np.matrix(moments).transpose()
     quadrature_coefficients = list(np.ravel(np.linalg.solve(knots_matrix, moments_vector)))
-    return knots, quadrature_coefficients
+    return knots, quadrature_coefficients, "standard"
+
+
+def stand_meth_err_estimation(a, b, knots):
+    x_ = sp.Symbol("x")
+    f = -sp.Abs(sp.diff(function(0, diff=True), sp.Symbol("x"), len(knots)))
+    f = sp.utilities.lambdify(x_, f)
+    return (-optimize.minimize_scalar(f, bounds=(a, b), method='Bounded').fun / math.factorial(len(knots))) * \
+        integrate.quad(estimator_func, a, b, args=(knots,))[0]
 
 
 def gauss_iqr(a, b, n):
@@ -91,110 +99,99 @@ def gauss_iqr(a, b, n):
             knots_matrix[s][j] = knots[j] ** s
     moments_vector = np.matrix(moments[:n]).transpose()
     quadrature_coefficients = list(np.ravel(np.linalg.solve(knots_matrix, moments_vector)))
-    return knots, quadrature_coefficients
+    return knots, quadrature_coefficients, "gauss"
+
+
+def gauss_meth_err_estimation(a, b, knots):
+    x_ = sp.Symbol("x")
+    f = -sp.Abs(sp.diff(function(0, diff=True), sp.Symbol("x"), 2*len(knots)))
+    f = sp.utilities.lambdify(x_, f)
+    return (-optimize.minimize_scalar(f, bounds=(a, b), method='Bounded').fun / math.factorial(2*len(knots))) * \
+        integrate.quad(estimator_func, a, b, args=(knots, 2))[0]
 
 
 def interpolation_quadrature_rules(a, b, n, method=regular_iqr, composite=False):
     numerical_value = 0
-    knots, quadrature_coefficients = method(a, b, n)
+    estimation_methods = {"standard": stand_meth_err_estimation, "gauss": gauss_meth_err_estimation}
+    knots, quadrature_coefficients, est = method(a, b, n)
     for i in range(n):
         numerical_value += quadrature_coefficients[i] * function(knots[i])
     if not composite:
         sample_value = integrate.quad(wanted_function, a, b)[0]
         exact_error = abs(sample_value - numerical_value)
-        methodical_error = methodical_error_estimation(a, b, knots)
-        errors_difference = abs(exact_error - methodical_error)
+        methodical_error = estimation_methods[est](a, b, knots)
         print("Quadrature coefficients: ", quadrature_coefficients,
-              "\nExact quadrature coefficients", coefficients(a, b, knots))
+              "\nExact quadrature coefficients: ", coefficients(a, b, knots))
         print("Resulting value: ", numerical_value, "\nExact value: ", sample_value, "\nMethodical Error: ",
-              methodical_error, "\nExact error: ", exact_error, "\nDifference between methodical and exact errors: ",
-              errors_difference)
+              methodical_error, "\nExact error: ", exact_error)
     return numerical_value
 
 
-def methodical_error_estimation(a, b, knots):
-    x_ = sp.Symbol("x")
-    f = -sp.Abs(sp.diff(function(0, diff=True), sp.Symbol("x"), len(knots)))
-    f = sp.utilities.lambdify(x_, f)
-    return (-optimize.minimize_scalar(f, bounds=(a, b), method='Bounded').fun / math.factorial(len(knots))) * \
-        integrate.quad(estimator_func, a, b, args=(knots,))[0]
+def s_h_values(a, b, n, k, method=regular_iqr):
+    h = (b - a) / k
+    s_h = 0
+    for i in range(k):
+        s_h += interpolation_quadrature_rules(a + i * h, a + (i + 1) * h, n, method=method, composite=True)
+    return s_h
 
 
 def runge(a, b, n, epsilon=10**-6, method=regular_iqr):
-    s_h1 = 0
-    s_h2 = 0
-    k = 3
-    l_ = 2
-    m = aitken(a, b, n)
-    h = (b - a) / k
-    for i in range(k):
-        s_h1 += interpolation_quadrature_rules(a + i * h, a + (i + 1) * h, n, method=method, composite=True)
-        for j in range(l_):
-            s_h2 += interpolation_quadrature_rules(a + (i * l_ + j) * h / l_, a + (i * l_ + j + 1) * h / l_, n,
-                                                   method=method, composite=True)
-    h_opt = h * ((epsilon * (1 - l_ ** (-m)) / abs(s_h2 - s_h1)) ** (1 / m))
-    k_opt = math.floor((b - a) / h_opt)
-    return h_opt, k_opt
-
-
-def richardson(a, b, n, epsilon=10**-6, method=regular_iqr):
-    sample_value = integrate.quad(wanted_function, a, b)[0]
-    s_h1 = 0
-    s_h2 = 0
     k = 3
     l_ = 2
     m = 2
-    h_1 = (b - a) / k
-    h_2 = h_1 / l_
-    for i in range(k):
-        s_h1 += interpolation_quadrature_rules(a + i * h_1, a + (i + 1) * h_1, n, method=method, composite=True)
-        for j in range(l_):
-            s_h2 += interpolation_quadrature_rules(a + (i * l_ + j) * h_2, a + (i * l_ + j + 1) * h_2, n,
-                                                   method=method, composite=True)
-    m = aitken(a, b, n)
-    res = s_h2 + (s_h2 - s_h1)/(l_ ** m - 1)
-    print("Richardson value: ", res, "\nRichardson error: ", sample_value - res)
-    return res
+    h = (b - a) / k
+    s_h1, s_h2 = s_h_values(a, b, n, k, method=method), s_h_values(a, b, n, k * l_, method=method)
+    h_opt = h * ((epsilon * (1 - l_ ** (-m)) / abs(s_h2 - s_h1)) ** (1 / m))
+    k_opt = math.floor((b - a) / h_opt)
+    h_opt = (b - a) / k_opt
+    return h_opt, k_opt
+
+
+def richardson(a, b, n, r, method=regular_iqr):
+    sample_value = integrate.quad(wanted_function, a, b)[0]
+    r = 8  # hard-coding
+    l_ = 2
+    m = aitken(a, b, n, method=method)
+    h = (b - a) / r
+    h_matrix = np.zeros((r + 1, r + 1))
+    s_h_vector = []
+    for i in range(r+1):
+        for j in range(r+1):
+            if j == r:
+                h_matrix[i][j] = -1
+            else:
+                h_matrix[i][j] = (h/l_**i)**(m+j)
+        s_h_vector.append(- s_h_values(a, b, n, r*l_**i, method=method))
+    s_h_vector = np.matrix(s_h_vector).transpose()
+    coefficients_ = list(np.ravel(np.linalg.solve(h_matrix, s_h_vector)))
+    error = abs(np.ravel(coefficients_[-1] + s_h_vector[-1])[0])
+    h_opt, k_opt = (b - a) / (r * l_ ** r), r * l_ ** r
+    return error
 
 
 def aitken(a, b, n, method=regular_iqr):
-    s_h1 = 0
-    s_h2 = 0
-    s_h3 = 0
     k = 3
     l_ = 2
-    h_1 = (b - a) / k
-    h_2 = h_1 / l_
-    h_3 = h_2 / l_
-    for i in range(k):
-        s_h1 += interpolation_quadrature_rules(a + i * h_1, a + (i + 1) * h_1, n, method=method, composite=True)
-        for j in range(l_):
-            s_h2 += interpolation_quadrature_rules(a + (i * l_ + j) * h_2, a + (i * l_ + j + 1) * h_2, n,
-                                                   method=method, composite=True)
-            for _ in range(l_):
-                s_h3 += interpolation_quadrature_rules(a + (i * l_ ** 2 + j * l_ + _) * h_3,
-                                                       a + (i * l_ ** 2 + j * l_ + _ + 1) * h_3, n,
-                                                       method=method, composite=True)
+    s_h1, s_h2, s_h3 = s_h_values(a, b, n, k, method=method), s_h_values(a, b, n, k * l_, method=method), \
+        s_h_values(a, b, n, k * l_**2, method=method)
     m = - (np.log(abs((s_h3 - s_h2)/(s_h2 - s_h1)))/np.log(l_))
     return m
 
 
 def composite_quadrature_rules(a, b, n, method=regular_iqr, accuracy_rule=runge, epsilon=10**-6):
     sample_value = integrate.quad(wanted_function, a, b)[0]
-    numerical_value = 0
     h_opt, k_opt = accuracy_rule(a, b, n, epsilon=epsilon, method=method)
-    for j in range(k_opt):
-        numerical_value += interpolation_quadrature_rules(a + j * h_opt, a + (j + 1) * h_opt, n,
-                                                          method=method, composite=True)
-    numerical_value += interpolation_quadrature_rules(a + k_opt * h_opt, b, n, method=method, composite=True)
-    print("\nComposite resulting value: ", numerical_value, "\nDifference: ", abs(sample_value - numerical_value))
+    numerical_value = s_h_values(a, b, n, k_opt, method=method)
+    r_error = richardson(a, b, n, k_opt, regular_iqr)
+    print("\nComposite resulting value: ", numerical_value, "\nRichardson's error: ", r_error,
+          "\nExact error: ", abs(sample_value - numerical_value))
 
 
 def main(a, b, n):
     start = time.time()
     interpolation_quadrature_rules(a, b, n, method=regular_iqr)
     composite_quadrature_rules(a, b, n, method=regular_iqr, accuracy_rule=runge, epsilon=epsilon_)
-    print("Elapsed time: ", time.time() - start)
+    print("\nElapsed time: ", time.time() - start)
 
 
 if __name__ == "__main__":
